278fffd464c4df1ff17396a3df9a82e246d1a5c1
LeandroDanielL <leandro.daniel.lsoares@gmail.com>
2024-07-17 16:55:43 -0300

    Inclusão da classe constantes (#4)
    Inclusão da classe constantes

---
+++ lib/constantes.dart
@@ -0,0 +1,23 @@
+  // constante para as categorias presente no filtro 
+   Map<String, bool> categoriasFiltro = {
+    'Assistência social': false,
+    'Sem teto': false,
+    'Animais': false,
+    'Cultura': false,
+    'Ambientais': false,
+    'Saúde': false,
+    'Direitos humanos': false,
+};
+ 
+
+  // constante para os itens da loja 
+   List<Map<String, dynamic>> itensLoja = [
+    {'nome': 'Kit de Alimentos', 'preco': 123, 'imagem': 'assets/images/kit_alimentos.png', 'quantidade': 1},
+    {'nome': 'Kit de Higiene Completo', 'preco': 72, 'imagem': 'assets/images/kit_higiene_completo.png', 'quantidade': 1},
+    {'nome': 'Kit Higiene Feminino', 'preco': 36, 'imagem': 'assets/images/kit_higiene_feminino.png', 'quantidade': 1},
+    {'nome': 'Kit Higiene Masculino', 'preco': 29, 'imagem': 'assets/images/kit_higiene_masculino.png', 'quantidade': 1},
+    {'nome': 'Kit Ração Cao', 'preco': 97, 'imagem': 'assets/images/kit_racao_cao.png', 'quantidade': 1},
+    {'nome': 'Kit Ração Gato', 'preco': 140, 'imagem': 'assets/images/kit_racao_gato.png', 'quantidade': 1},
+    {'nome': 'Fralda Descartavel', 'preco': 78, 'imagem': 'assets/images/fralda_descartavel.png', 'quantidade': 1},
+  ];
+ 
